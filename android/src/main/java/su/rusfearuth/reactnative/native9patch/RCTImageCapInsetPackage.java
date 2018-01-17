5e00d3f3671124fd20f1f770237790c14f8ac2b3
Eric McConkie <eric@poynt.co>
2018-01-17 15:15:40 -0800

    update package to build for RN >= 42

--- android/src/main/java/su/rusfearuth/reactnative/native9patch/RCTImageCapInsetPackage.java
+++ android/src/main/java/su/rusfearuth/reactnative/native9patch/RCTImageCapInsetPackage.java
@@ -5,12 +5,21 @@ import com.facebook.react.bridge.JavaScriptModule;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
+
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 
 public class RCTImageCapInsetPackage implements ReactPackage {
+    @Override
+    public List<Class<? extends JavaScriptModule>> createJSModules() {
+        return new ArrayList<>();
+    }
+
+    /**/
+    /**/
     @Override
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
         return Collections.emptyList();
@@ -20,4 +29,5 @@ public class RCTImageCapInsetPackage implements ReactPackage {
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
         return Arrays.<ViewManager>asList(new RCTImageCapInsetManager());
     }
+    /**/
 }
